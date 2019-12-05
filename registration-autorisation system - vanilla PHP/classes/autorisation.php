fee1eaf4bc384b408c9dc9800ab85e0581898564
OkForg97 <okfrog97@yandex.ru>
2019-12-05 06:10:11 +0500

    04.12| add autorisation class

---
+++ registration-autorisation system - vanilla PHP/classes/autorisation.php
@@ -0,0 +1,41 @@
+<?php
+class autorisation
+{
+
+    protected $userLog;
+    protected $userPass;
+    protected $userAutorisationInfo;
+
+    function __construct($userLog, $userPass)
+    {
+        $this->userLog = $userLog;
+        $this->userPass = $userPass;
+        $this->userAutorisationInfo = $userLog . ':' . $userPass;
+    }
+
+    private function logFinder()
+    {
+        $handle = fopen("db.txt", "a+");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                if (stripos($line, $this->userLog) !== false) {
+                    $userData = explode(':', $line);
+                    return $userData;
+                }
+            }
+        }
+    }
+
+    public function getAutorisation ()
+    {
+
+        $userData = $this->logFinder();
+        if (($this->userLog === $userData[0]) && ($this->userPass === str_replace(PHP_EOL, '', $userData[1]))) {
+            session_start();
+            $_SESSION['autorisation'] = 'True';
+            return 'True';
+        } else {
+            return 'False';
+        }
+    }
+}
