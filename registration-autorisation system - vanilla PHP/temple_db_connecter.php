3404181868c0f42212962ab261ac90587c939f3c
OkForg97 <okfrog97@yandex.ru>
2019-12-11 06:36:02 +0500

    11.12| add test connection with MySQLi

--- registration-autorisation system - vanilla PHP/temple_db_connecter.php
+++ registration-autorisation system - vanilla PHP/temple_db_connecter.php
@@ -1,3 +1,17 @@
 <?php
 
 //test connection
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+
+$dbConn = new mysqli($servername, $username, $password,'test_shema');
+
+if ($dbConn->connect_error){
+    die('Connect filed: '.$dbConn->connect_error);
+}
+else {
+    $res = $dbConn->query("SELECT * FROM users");
+    var_dump($res->fetch_all());
+}
