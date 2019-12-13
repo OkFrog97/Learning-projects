31d483b542ab161643b91a6b684661d94da21366
OkForg97 <okfrog97@yandex.ru>
2019-12-13 07:06:56 +0500

    13.12| add new connection to db

--- registration-autorisation system - vanilla PHP/index.php
+++ registration-autorisation system - vanilla PHP/index.php
@@ -12,6 +12,7 @@
             <p><a href="content.php">Получить контент</a></p>
             <p><a href="autorisationPage.php">Авторизоваться</a></p>
             <p><a href="registrationPage.php">Зарегистрироваться</a></p>
+            <p><a href="temple_db_connecter.php">DB testing link</a></p>
         </nav>
 
 
--- registration-autorisation system - vanilla PHP/temple_db_connecter.php
+++ registration-autorisation system - vanilla PHP/temple_db_connecter.php
@@ -2,3 +2,18 @@
 
 //test connection
 
+// database settings
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=test_shema', $username, $password);
+    foreach($dbh->query('SELECT * from users') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
