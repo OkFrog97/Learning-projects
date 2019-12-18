cd69acfd9b9d871cea6dbec99113aaf6bcd38691
OkForg97 <okfrog97@yandex.ru>
2019-12-18 05:16:16 +0500

    18.12| add oops page(for non autorisation users)

---
+++ registration-autorisation system - vanilla PHP/oops.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Do Androids Dream of Electric Sheep?</title>
+</head>
+<body>
+<h2>Ууупс! Кто-то забыл авторизоваться!</h2>
+<p><a href="index.php">На главную</a></p>
+</body>
+</html>
