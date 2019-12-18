09b612b6b3f94adc0ed3990c51144ad468668b47
OkForg97 <okfrog97@yandex.ru>
2019-12-18 05:32:13 +0500

    18.12| add new autorisation logic with db

--- registration-autorisation system - vanilla PHP/classes/autorisation.php
+++ registration-autorisation system - vanilla PHP/classes/autorisation.php
@@ -6,6 +6,11 @@ class autorisation
     protected $userPass;
     protected $userAutorisationInfo;
 
+    //db connection settings
+    protected $localhost;
+    protected $username;
+    protected $password;
+
     function __construct($userLog, $userPass)
     {
         $this->userLog = $userLog;
@@ -17,8 +22,13 @@ class autorisation
     {
         /*
          * Return data from db.
-         * WOrk with real db must be here.
+         * Work with real db must be here.
          */
+
+        $dbh = new PDO('mysql:host=localhost;dbname=test_shema', $this->username, $this->password);
+        return $dbh;
+
+        /*
         $handle = fopen("db.txt", "a+");
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
@@ -28,8 +38,9 @@ class autorisation
                 }
             }
         }
-    }
+        */
 
+    }
     public function getAutorisation ()
     {
         /*
@@ -37,6 +48,33 @@ class autorisation
          * Should be boolean?
          */
         $userData = $this->logFinder();
+
+        foreach($userData->query('SELECT  username, password from users') as $row) {
+
+            //Проверяем построчно то, что вернула ДБ на соответствие логину и паролю
+            //Нужно найти более эффектиный способ
+            if (($row['username']===$this->userLog)&&($row['password']===$this->userPass)){
+                session_start();
+                $_SESSION['autorisation'] = 'True';
+                return 'True';
+            }
+            return "False";
+
+        }
+        $dbh = null;
+
+
+
+
+
+
+
+
+
+
+
+
+       /*
         if (($this->userLog === $userData[0]) && ($this->userPass === str_replace(PHP_EOL, '', $userData[1]))) {
             session_start();
             $_SESSION['autorisation'] = 'True';
@@ -44,5 +82,7 @@ class autorisation
         } else {
             return 'False';
         }
+       */
     }
+
 }
