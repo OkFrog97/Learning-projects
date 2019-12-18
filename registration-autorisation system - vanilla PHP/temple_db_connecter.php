bcd3bbf441777f0db571a6188725e37b4d170447
OkForg97 <okfrog97@yandex.ru>
2019-12-18 05:18:25 +0500

    18.12| return old way to check autorisation

--- registration-autorisation system - vanilla PHP/temple_db_connecter.php
+++ registration-autorisation system - vanilla PHP/temple_db_connecter.php
@@ -14,9 +14,6 @@ $pass = 'qwerty';
 
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=test_shema', $username, $password);
-    $result = $dbh->query('SELECT  username, password from users');
-    print_r($result); // так возвращается какой-то объект и данных в нем наверн не найти
-    /*
     foreach($dbh->query('SELECT  username, password from users') as $row) {
 
         //Проверяем построчно то, что вернула ДБ на соответствие логину и паролю
@@ -29,7 +26,6 @@ try {
         }
 
     }
-    */
     $dbh = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
