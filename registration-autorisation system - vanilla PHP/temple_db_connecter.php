472da18c8188c3e382479ee616f40133d5fc94ad
OkForg97 <okfrog97@yandex.ru>
2019-12-20 05:49:50 +0500

    20.12| add to db test function

--- registration-autorisation system - vanilla PHP/temple_db_connecter.php
+++ registration-autorisation system - vanilla PHP/temple_db_connecter.php
@@ -11,13 +11,24 @@ $password = '';
 $login = 'fred';
 $pass = 'qwerty';
 
-
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=test_shema', $username, $password);
 
+    $stmp = $dbh->prepare("INSERT INTO users VALUES (:username, :password)");
+    $stmp->bindParam(':username', $addname);
+    $stmp->bindParam(':password', $addpass);
 
+    //test added values 1
+    $addname = 'nagibator';
+    $addpass = '123456789';
+
+    //test added values 2
+    $addname = 'nagibator';
+    $addpass = '123456789';
 
     $dbh = null;
+
+    echo '<p>data add to database</p><br>';
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
