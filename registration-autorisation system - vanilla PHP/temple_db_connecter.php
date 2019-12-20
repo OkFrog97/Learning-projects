3d98e2b97cff5a92e5f80fb1c596982f3d402cfe
OkForg97 <okfrog97@yandex.ru>
2019-12-20 05:35:48 +0500

    20.12| add to db test function

--- registration-autorisation system - vanilla PHP/temple_db_connecter.php
+++ registration-autorisation system - vanilla PHP/temple_db_connecter.php
@@ -12,24 +12,13 @@ $login = 'fred';
 $pass = 'qwerty';
 
 
-/*
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=test_shema', $username, $password);
-    foreach($dbh->query('SELECT  username, password from users') as $row) {
 
-        //Проверяем построчно то, что вернула ДБ на соответствие логину и паролю
-        //Нужно найти более эффектиный способ
-        if (($row['username']===$login)&&($row['password']===$pass)){
-            print_r("True! <br>"); // Во встроенной функции тут должен быть ретурнж
-        }
-        else {
-            print_r("False! <br>");
-        }
 
-    }
+
     $dbh = null;
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-*/
