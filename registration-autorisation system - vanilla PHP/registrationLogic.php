49138b0c74add2a6cc3f229fef7ae805448d3f3b
OkForg97 <okfrog97@yandex.ru>
2019-12-21 19:47:22 +0500

    21.12| try to start registration again

--- registration-autorisation system - vanilla PHP/registrationLogic.php
+++ registration-autorisation system - vanilla PHP/registrationLogic.php
@@ -1,5 +1,13 @@
 <?php
 //автозагружаем класс
+spl_autoload_register( function ($class_name){
+    include 'classes'.DIRECTORY_SEPARATOR.$class_name . '.php';
+});
+
+$userLog = $_POST['log'];
+$userPass = $_POST['pass'];
+
+//registration function
 function setRegistration($log, $pass){
 
     // database settings
