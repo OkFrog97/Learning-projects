a88861c6ae90756250b691c30ee4d3825a5443d5
OkForg97 <okfrog97@yandex.ru>
2019-12-21 19:35:10 +0500

    21.12| add registration complete function

--- registration-autorisation system - vanilla PHP/autorisationLogic.php
+++ registration-autorisation system - vanilla PHP/autorisationLogic.php
@@ -11,8 +11,11 @@ $userPass = $_POST['pass'];
 
 function setRegistration($log, $pass){
 
+    // database settings
+    $username = 'root';
+    $password = '';
 
-
+    //db function
     try {
         $dbh = new PDO('mysql:host=localhost;dbname=test_shema', $username, $password);
 
@@ -21,18 +24,17 @@ function setRegistration($log, $pass){
 
         //вставляем параменты запроса и отправляем его
         $params = [
-            ':username'=> 'jehny',
-            ':password'=> '0987654321',
+            ':username'=> $log,
+            ':password'=> $pass,
         ];
         $stmp->execute($params);
-
         $dbh = null;
-
         echo '<p>data add to database</p><br>';
 
     } catch (PDOException $e) {
         print "Error!: " . $e->getMessage() . "<br/>";
         die();
+    }
 
 
 
