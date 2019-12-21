174156920df17977d78f6dad611a38ca131690f3
OkForg97 <okfrog97@yandex.ru>
2019-12-21 19:31:42 +0500

    21.12| add registration raw function

--- registration-autorisation system - vanilla PHP/autorisationLogic.php
+++ registration-autorisation system - vanilla PHP/autorisationLogic.php
@@ -13,7 +13,26 @@ function setRegistration($log, $pass){
 
 
 
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=test_shema', $username, $password);
 
+        //Готовим запрос
+        $stmp = $dbh->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+
+        //вставляем параменты запроса и отправляем его
+        $params = [
+            ':username'=> 'jehny',
+            ':password'=> '0987654321',
+        ];
+        $stmp->execute($params);
+
+        $dbh = null;
+
+        echo '<p>data add to database</p><br>';
+
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
 
 
 
