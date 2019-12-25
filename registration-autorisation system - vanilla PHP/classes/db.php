bebaa83e38194474421aad87c4cad63222bdd370
OkForg97 <okfrog97@yandex.ru>
2019-12-25 07:26:06 +0500

    25.12| add db autorisation method

--- registration-autorisation system - vanilla PHP/classes/db.php
+++ registration-autorisation system - vanilla PHP/classes/db.php
@@ -9,5 +9,47 @@ class db
     private $password = '';
 
 
+    public function getAutorisation($log, $pass){
+
+        //db function Вместо всего этого вернуть массив логинов и паролей или просто сравнить полученные
+        try {
+            $dbh = new PDO('mysql:host=localhost;dbname=test_shema', $username, $password);
+            foreach($dbh->query('SELECT username, password FROM users;') as $row){
+                if ( ($row['username']==$log)&&($row['password'])==$pass){
+
+                    //работа с сессиями ВРЕМЕННО
+                    session_start();
+                    $_SESSION['autorisation'] = 'True';
+
+                    return 'True';
+                }
+            }
+            return "False";
+
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
