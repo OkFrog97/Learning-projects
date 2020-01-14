7d00a1a898b05929e023454e452161ba5ac58469
OkForg97 <okfrog97@yandex.ru>
2020-01-14 07:17:18 +0500

    14.01| ToDo list| add  basic parameters

--- toDo list/index.php
+++ toDo list/index.php
@@ -4,6 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, instal-scale=1.0">
         <title>ToDo list</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     </head>
     <body>
 
