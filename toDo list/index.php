ce7d16b4dfe56987e262601dc465085a3eafec24
OkForg97 <okfrog97@yandex.ru>
2020-01-14 07:06:54 +0500

    14.01| ToDo list| add  basic html structure

---
+++ toDo list/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang ="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, instal-scale=1.0">
+        <title>ToDo list</title>
+    </head>
+    <body>
+
+    </body>
+
+
+</html>
