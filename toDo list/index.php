44b0eb2cbce0e84c4ce781456fa116cba4224e8c
OkForg97 <okfrog97@yandex.ru>
2020-01-15 06:23:09 +0500

    14.01| ToDo list| fix bug with connection with style.css

--- toDo list/index.php
+++ toDo list/index.php
@@ -2,14 +2,15 @@
 <html lang ="ru">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, instal-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>ToDo list</title>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="style.css"> <!--Не подключается из папки. Почему? -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     </head>
     <body>
 
-    <div calss="container">
+    <div class="container">
         <h1>ToDo list</h1>
         <!--Переделать с использованием ajax!-->
         <form action="/add.php" method="post">
