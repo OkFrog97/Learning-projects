d201658baece8dd115c21b7ecb390d290589d6f4
OkForg97 <okfrog97@yandex.ru>
2020-01-16 07:33:47 +0500

    16.01| ToDo list| add main function

--- toDo list/add.php
+++ toDo list/add.php
@@ -1,9 +1,5 @@
 <?php
 
-//get user task information
-$task = $_POST["task"];
-
-
 //functions
 function isValid ($sendData)
 {
@@ -16,12 +12,35 @@ function isValid ($sendData)
 
 
 function dbConnect ($sendData){
+    //vars
     $dsn = "mysql:host=localhost;dbname=todo_list";
     $pdo = new PDO ($dsn, 'root', '');
-
     $sql = 'INSERT INTO tasks(task) VALUES(:task)';
 
+    //logic
+    try {
+        $query = $pdo->prepare($sql);
+        $query->execute(['task' => $sendData]);
+    }
+    catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+}
 
+
+function main ()
+{
+    //get user task information
+    $task = $_POST["task"];
+
+    //db connection
+
+
+    //redirect to the index
+    header ('Location: /');
 
 
 }
+
+main();
