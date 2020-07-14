d42e6f75302027a6cf13a237206f0e47840bfe22
OkForg97 <okfrog97@yandex.ru>
2020-07-14 06:17:42 +0500

    SOL| simpleObj and thisObj

--- js_scripts/sicretObjectLife/script.js
+++ js_scripts/sicretObjectLife/script.js
@@ -1,6 +1,23 @@
 window.addEventListener("load", function(){
-    //Тайная жизнь объектов! Ууууу!
+    // Тайная жизнь объектов! Ууууу!
+    // Погружаемся в Объектный джаваскрипт!
 
+    //Инкапсуляция
 
+    //simple rabbit object
+    let rabbit = {};
+    rabbit.speak = function (line){
+        console.log(`Кролик говорит: ${line}!`);
+    };
+    rabbit.speak("Я живой");
+
+    //this указывает на объект, функция которого была вызвана. Т.е. на себя
+    function speak (line) {
+        console.log(`${this.type} кролик говорит: ${line}!`);
+    };
+
+    let whiteRabbit = {type: "Белый", speak}; //speak написана как отдельная функция и передана в js
+    whiteRabbit.speak("БаксБанни просто сосунок");
+    
     
 });
