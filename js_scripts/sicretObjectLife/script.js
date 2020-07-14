1849b294cae1b40c27395b30e81a4f78107a9d2d
OkForg97 <okfrog97@yandex.ru>
2020-07-14 06:53:36 +0500

    SOL| prototypes

--- js_scripts/sicretObjectLife/script.js
+++ js_scripts/sicretObjectLife/script.js
@@ -18,6 +18,34 @@ window.addEventListener("load", function(){
 
     let whiteRabbit = {type: "Белый", speak}; //speak написана как отдельная функция и передана в js
     whiteRabbit.speak("БаксБанни просто сосунок");
-    
+
+    // this и стрелочная функция: стрел.функ. не имеют своей th но могут видеть 
+    // привязку внутри области видимости в которой они находятся
+
+    function normalize () {
+        console.log(this.coords.map(n => n / this.lenght));
+    }
+    normalize.call({coords: [0, 2, 5], lenght: 5});
+
+    /*  call явно указывает на объект
+     *  В ф. normalize указали на поле кордс передаваемого объекта
+     *  и задали мап с анонимной функцией
+     */
+
+    // ПРОТОТИПЫ
+    // Прототип - запасной объект-источник свойств, родитель объекта;
+    // Object.prototype - Всеотец всех объектов.
+
+    let protoRabbit = {
+        speak (line) {
+            console.log(`${this.type} кролик говорит: ${line}!`);
+        }        
+    };
+
+    let killerRabbit = Object.create(protoRabbit); //явно указали прототип кролика
+    killerRabbit.type = "Зайка-Убивайка";
+    killerRabbit.speak("(л)Убить всех человеков, кроме Мазая!"); //метод из прототипа;
+
+
     
 });
