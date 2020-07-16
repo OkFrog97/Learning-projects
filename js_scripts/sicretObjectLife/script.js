cfa0797a0a8880170c3a3eab0a76187a1547fad8
OkForg97 <okfrog97@yandex.ru>
2020-07-16 07:04:30 +0500

    SOL | add matrixIterator class

--- js_scripts/sicretObjectLife/script.js
+++ js_scripts/sicretObjectLife/script.js
@@ -88,15 +88,12 @@ window.addEventListener("load", function(){
             this.width = width;
             this.height = height;
             this.content = [];
-
-
             for (let y = 0; y < height; y++){
                 for (let x = 0; x < height; x++){
                     this.content[y * width + x] = element(x, y); 
                 }
             }
         }
-
         get (x, y) {
             return this.content[y * width + x];
         }
@@ -106,6 +103,29 @@ window.addEventListener("load", function(){
         }
     }
 
+    class MatrixIterator {
+        constructor (matrix){
+            this.x = 0;
+            this.y = 0;
+            this.matrix = matrix;
+        }
+
+        next() {
+            if (this.y === this.matrix.height) return {done: true};
+
+            let value = {x : this.x,
+                         y : this.y,
+                         value : this.matrix.get(this.x, this.y),
+                        };
+            this.x ++;
+
+            if (this.x == this.matrix.width) {
+                this.x = 0;
+                this.y++;
+            }
+            return {value, done: false};
+        }
+    }
 
 
     
